dlang/phobos

Cleanup temp files in std.process and std.experimental.checkedint
Co-Authored-By: Bernd Edlinger <bernd.edlinger@hotmail.de>

std/experimental/checkedint.d
@@ -1768,9 +1768,11 @@ static:
     auto y = checked!Abort(-1);
 
     // Temporarily redirect output to stderr to make sure we get the right output.
+    import std.file : exists, remove;
     import std.process : uniqueTempPath;
     import std.stdio : stderr;
     auto tmpname = uniqueTempPath;
+    scope(exit) if (exists(tmpname)) remove(tmpname);
     auto t = stderr;
     stderr.open(tmpname, "w");
     // Open a new scope to minimize code ran with stderr redirected.

std/process.d
@@ -1880,6 +1880,7 @@ version (Windows)
 @system unittest
 {
     auto fn = uniqueTempPath();
+    scope(exit) if (exists(fn)) remove(fn);
     std.file.write(fn, "AAAAAAAAAA");
 
     auto f = File(fn, "a");
@@ -1894,11 +1895,12 @@ version (Windows)
 // with indicating a workDir.
 version (Posix) @system unittest
 {
-    import std.file : mkdir, write, setAttributes;
+    import std.file : mkdir, write, setAttributes, rmdirRecurse;
     import std.conv : octal;
 
     auto dir = uniqueTempPath();
     mkdir(dir);
+    scope(exit) rmdirRecurse(dir);
     write(dir ~ "/program", "#!/bin/sh\necho Hello");
     setAttributes(dir ~ "/program", octal!700);
 
@@ -3357,11 +3359,12 @@ private auto executeImpl(alias pipeFunc, Cmd, ExtraPipeFuncArgs...)(
     // Temporarily disable output to stderr so as to not spam the build log.
     import std.stdio : stderr;
     import std.typecons : Tuple;
-    import std.file : readText;
+    import std.file : readText, exists, remove;
     import std.traits : ReturnType;
 
     ReturnType!executeShell r;
     auto tmpname = uniqueTempPath;
+    scope(exit) if (exists(tmpname)) remove(tmpname);
     auto t = stderr;
     // Open a new scope to minimize code ran with stderr redirected.
     {

epslon-dlang/riphobos

Signed-off-by: Luís Ferreira <contact@lsferreira.net>

source/ripstd/experimental/checkedint.d
@@ -1768,9 +1768,11 @@ static:
     auto y = checked!Abort(-1);
 
     // Temporarily redirect output to stderr to make sure we get the right output.
+    import ripstd.file : exists, remove;
     import ripstd.process : uniqueTempPath;
     import ripstd.stdio : stderr;
     auto tmpname = uniqueTempPath;
+    scope(exit) if (exists(tmpname)) remove(tmpname);
     auto t = stderr;
     stderr.open(tmpname, "w");
     // Open a new scope to minimize code ran with stderr redirected.

source/ripstd/process.d
@@ -1880,6 +1880,7 @@ version (Windows)
 @system unittest
 {
     auto fn = uniqueTempPath();
+    scope(exit) if (exists(fn)) remove(fn);
     ripstd.file.write(fn, "AAAAAAAAAA");
 
     auto f = File(fn, "a");
@@ -1894,11 +1895,12 @@ version (Windows)
 // with indicating a workDir.
 version (Posix) @system unittest
 {
-    import ripstd.file : mkdir, write, setAttributes;
+    import ripstd.file : mkdir, write, setAttributes, rmdirRecurse;
     import ripstd.conv : octal;
 
     auto dir = uniqueTempPath();
     mkdir(dir);
+    scope(exit) rmdirRecurse(dir);
     write(dir ~ "/program", "#!/bin/sh\necho Hello");
     setAttributes(dir ~ "/program", octal!700);
 
@@ -3383,11 +3385,12 @@ private auto executeImpl(alias pipeFunc, Cmd, ExtraPipeFuncArgs...)(
     // Temporarily disable output to stderr so as to not spam the build log.
     import ripstd.stdio : stderr;
     import ripstd.typecons : Tuple;
-    import ripstd.file : readText;
+    import ripstd.file : readText, exists, remove;
     import ripstd.traits : ReturnType;
 
     ReturnType!executeShell r;
     auto tmpname = uniqueTempPath;
+    scope(exit) if (exists(tmpname)) remove(tmpname);
     auto t = stderr;
     // Open a new scope to minimize code ran with stderr redirected.
     {