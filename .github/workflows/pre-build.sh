d0b5fb3c6d3499037bdb9cb1fdda299631053ac5
Luís Ferreira <contact@lsferreira.net>
2021-08-31 18:37:02 +0100

    github: workflows: fix pre-build script shellcheck issues
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

--- .github/workflows/pre-build.sh
+++ .github/workflows/pre-build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [  -n "$(uname -a | grep Ubuntu)" ]; then # Ubuntu
+if uname -a | grep -q Ubuntu; then # Ubuntu
     sudo apt-get update
     sudo apt-get install -y libcurl4-openssl-dev
 fi
